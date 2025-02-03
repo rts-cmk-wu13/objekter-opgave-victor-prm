62ede21777a2a39ba67480b6e173f37445c6aabc
victor-prm <victorpabb@gmail.com>
2025-02-03 14:29:23 +0100

    added some basic html with js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
+
+let headerImageContainer = document.createElement('div');
+headerImageContainer.classList.add('header-image-container')
+headerImageContainer.innerHTML = `<img src=${hero.image} alt="hero image" class="header-image-image">`;
+document.querySelector('.header').append(headerImageContainer)
+
+let headerTextContainer = document.createElement('div');
+headerTextContainer.classList.add('header-text-container')
+headerTextContainer.innerHTML = `
+<h1 class="header-text-headline">${hero.headline}</h1>
+<p class="header-text-copy">${hero.copy}</p>`
+document.querySelector('.header').append(headerTextContainer)
